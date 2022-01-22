333008d5b47c40e1d11139d819971b519945b439
user <user@iMac-user7.local>
2022-01-22 11:20:03 +0400

    add bottom navigation

---
+++ app/src/main/java/com/example/sask/ui/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.sask.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.sask.R
+import com.example.sask.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
